ef0bc263889c87314508c76a08c8f7d0be9a66b4
Oleksandr Hanchevskyi <ipz222_goo@student.ztu.edu.ua>
2022-11-12 12:54:20 +0300

    project6

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("123");
 Console.WriteLine("123");
 Console.WriteLine("123");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("123");
+Console.WriteLine("123");
